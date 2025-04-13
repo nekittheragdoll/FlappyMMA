d19ede7e7f2b3f034e44c74cd01d7437f44ddddd
nekittheragdoll <mykyta.syskov@seznam.cz>
2025-04-14 00:40:20 +0200

    Redesigned Obstacles

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var player: CharacterBody2D
-var obstacle_scene: Resource
+@export var player: CharacterBody2D
+@export var obstacle_scene: Resource
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,16 +18,25 @@ func _on_obstacle_hit(body: Node2D) -> void:
 	if (body == player):
 		get_tree().paused = true
 
+func _on_greenline_touch(body: Node2D) -> void:
+	print("PASSED")
+
 
 func _spawn_obstacle() -> void:
-	var obstacle_bot = _obstacle_creator(obstacle_scene, Vector2(800,900))
-	var obstacle_top = _obstacle_creator(obstacle_scene, Vector2(800,0))
-	add_child(obstacle_top)
-	add_child(obstacle_bot)
+	var obstacle = _obstacle_creator(obstacle_scene, 900)
+	add_child(obstacle)
 	
 	
-func _obstacle_creator(obstacle_scene:Resource, position: Vector2) -> Area2D:
-	var obstacle:Area2D = obstacle_scene.instantiate()
-	obstacle.position = position
-	obstacle.body_entered.connect(_on_obstacle_hit)
+func _obstacle_creator(obstacle_scene:Resource, position: int) -> Area2D:
+	var obstacle:Node2D = obstacle_scene.instantiate()
+	obstacle.position.x = position
+	for part in obstacle.get_children():
+		if part is not Area2D:continue
+		if part.name == "TopPart" or part.name == "BottomPart":
+			part.body_entered.connect(_on_obstacle_hit)
+		else:
+			part.body_entered.connect(_on_greenline_touch)
+	
+	#obstacle.body_entered.connect(_on_obstacle_hit)
+	
 	return obstacle
--- scripts/obstacle.gd
+++ scripts/obstacle.gd
@@ -1,16 +1,23 @@
-extends Area2D
+extends Node2D
 
 @export var speed : float = 100
+var TopPart:Area2D
+var BottomPart:Area2D
+var rng=RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	TopPart = get_node("TopPart")
+	BottomPart = get_node("BottomPart")
+
+	TopPart.position.y -= rng.randi_range(-20,200)
+	BottomPart.position.y += rng.randi_range(-20,200)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	position.x -= speed * delta
 	
-	if position.x < -100:
+	if position.x < -500:
 		queue_free()
 		pass
